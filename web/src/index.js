cc9d800d13912fa8a01c8d0936126e552e0a5cbc
dhmemi <dhmemi@gmail.com>
2021-09-28 00:23:12 +0800

    test check item

--- web/src/index.js
+++ web/src/index.js
@@ -1,9 +1,31 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
-function App() {
-  return <Button variant="contained">Hello World</Button>;
+class ItemCheck extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <FormControl component="fieldset">
+        <FormGroup aria-label="position" row>
+          <FormControlLabel
+            value="start"
+            control={<Switch color="primary" />}
+            label={this.props.label}
+            labelPlacement="start"
+          />
+        </FormGroup>
+      </FormControl>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ItemCheck label="start label" />, document.getElementById("root"));
